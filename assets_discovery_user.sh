d3fb766f137b1bd745e821f22af12f23cb979cb1
alu0101339542 <alu0101339542@ull.edu.es>
2023-05-09 12:18:12 +0100

    V2

--- assets_discovery_user.sh
+++ assets_discovery_user.sh
@@ -9,13 +9,28 @@ fi
 #Creación del usuario
 # Comprobación si el usuario ya existe
 if id "discovery" >/dev/null 2>&1; then
-	echo -e "El usuario discovery ya existe. Procediendo a quitarle permisos de escritura... \n"
+	echo -e "Procediendo a quitarle permisos de escritura al usuario discovery. \n"
 else
-	useradd discovery;
+	read -s -p "Introduzca una contraseña para el usuario discovery: " password1
+	echo
+
+	read -s -p "Re-escriba su contraseña: " password2
+	echo
+
+	# Comparar passwords
+	while [[ "$password1" != "$password2" ]]; do
+    		echo "Las contraseñas no coinciden."
+    		read -s -p "Introduzca la contraseña: " password1
+    		echo
+    		read -s -p "Re-escriba su contraseña:" password2
+    		echo
+	done
+
+	useradd discovery -p $password1;
 fi
 
 #Eliminación de privilegios al usuario
-discovery_privileges=$(sudo find / -type f -perm /u=w -user discovery 2> /dev/null)
+discovery_privileges=$(find / -type f -perm /u=w -user discovery 2> /dev/null)
 echo -e "Archivos con permiso de escritura para discovery: \n $discovery_privileges \n \nEliminando...\n"
 
 while IFS= read -r file; do
